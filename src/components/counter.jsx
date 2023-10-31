072607b08de14126818edc35733b7a9c28972e36
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2023-10-31 19:35:06 +0300

    feat: add prettier and first component

---
+++ src/components/counter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Counter = () => {
+    return (
+        <>
+            <h1>Counter</h1>
+            <button>Increment</button>
+        </>
+    );
+};
+
+export default Counter;
